0def250e676fc5ed5b57a0b6a8facdb298126b3c
pankaj suthar <146710838+Pankaj-suthar777@users.noreply.github.com>
2024-01-08 13:54:50 +0530

    Update index.js

--- client/src/pages/Profile/Products/index.js
+++ client/src/pages/Profile/Products/index.js
@@ -6,6 +6,7 @@ import { SetLoader } from "../../../redux/loadersSlice";
 import { DeleteProduct, GetProducts } from "../../../apicalls/product";
 import moment from "moment";
 import Bids from "./Bids";
+import Divider from "../../../components/Divider";
 
 function Products() {
   const [showBidsModal, setShowBidsModal] = useState();
@@ -68,42 +69,48 @@ function Products() {
           products.map((item) => {
             return (
               <div>
-                <div className="flex flex-col md:flex-row border justify-between border-solid p-2 rounded-[10px]">
-                  <div className="break-all break-words justify-between">
-                    <b>Name : </b>
-                    {item.name}
-                    <p>
-                      <b>Description : </b>
-                      {item.description}
-                    </p>
-                    <p>
-                      <b>Age : </b>
-                      {item.age}
-                    </p>
-                    <p>
-                      <b>Price : </b>
-                      {item.price}rs
-                    </p>
-
-                    <p>
-                      <b>Category : </b>
-                      {item.category}
-                    </p>
-                  </div>
-                  <div className="flex flex-col items-center justify-center mt-2">
-                    <p>
-                      <b>Stataus : </b>
-                      {item.status}
-                    </p>
+                <div className="flex flex-col border justify-between border-solid  rounded-[10px] p-2">
+                  <div className="bg-gray-300 h-10 flex-col flex  justify-center items-center rounded-lg">
                     <p>
                       <b>Created At : </b>
                       {moment(item.createdAt).format("DD-MM-YY hh:mm A")}
                     </p>
                   </div>
+                  <div className="flex p-3">
+                    <img
+                      alt=""
+                      className="h-36 w-36 object-cover"
+                      src={item.images[0]}
+                    ></img>
+
+                    <div className="break-all flex flex-col break-words text-sm  justify-between px-3">
+                      <p>
+                        <b>Name : </b>
+                        {item.name}
+                      </p>
+                      <p>
+                        <b>Age : </b>
+                        {item.age}
+                      </p>
+                      <p>
+                        <b>Price : </b>
+                        {item.price}rs
+                      </p>
+
+                      <p>
+                        <b>Category : </b>
+                        {item.category}
+                      </p>
+                      <p>
+                        <b>Stataus : </b>
+                        {item.status}
+                      </p>
+                    </div>
+                  </div>
 
                   <div className="flex gap-5 items-center ">
                     <i
-                      className="ri-delete-bin-line text-2xl cursor-pointer"
+                      className="ri-delete-bin-line  text-2xl cursor-pointer"
                       onClick={() => deleteProduct(item._id)}
                     ></i>
                     <i
